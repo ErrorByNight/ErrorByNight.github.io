ea21f7b691724aeb13bc4d07de7aff95e4d02418
ErrorByNight <41594357+ErrorByNight@users.noreply.github.com>
2021-05-07 13:57:01 +0100

    load

--- story.js
+++ story.js
@@ -8,11 +8,23 @@ var die = new Audio('die.mp3');
 function save(){
     localStorage.setItem('currentLocation', JSON.stringify(currentLocation));
     localStorage.setItem('inventory', JSON.stringify(inventory));
+    localStorage.setItem('locations[currentLocation].description', JSON.stringify(locations[currentLocation].description));
+    localStorage.setItem('locations[currentLocation].image', JSON.stringify(locations[currentLocation].image));
+    localStorage.setItem('locations[currentLocation].place', JSON.stringify(locations[currentLocation].place));
+    localStorage.setItem('locations[currentLocation].buttonchoice.choice1', JSON.stringify(locations[currentLocation].buttonchoice.choice1));
+    localStorage.setItem('locations[currentLocation].buttonchoice.choice2', JSON.stringify(locations[currentLocation].buttonchoice.choice2));
+    localStorage.setItem('locations[currentLocation].buttonchoice.choice3', JSON.stringify(locations[currentLocation].buttonchoice.choice3));
 }
 
 function load(){
     currentLocation = JSON.parse(localStorage.getItem('currentLocation'));
     inventory = JSON.parse(localStorage.getItem('inventory'));
+    $('#game-text').empty().append("<p>" + locations[currentLocation].description + "</p>");
+    $('#game-image').empty().append(locations[currentLocation].image);
+    $('#game-place').empty().append(locations[currentLocation].place);
+    $('#gamebuttons').empty().append(locations[currentLocation].buttonchoice.choice1);
+    $('#gamebuttons').append(locations[currentLocation].buttonchoice.choice2);
+    $('#gamebuttons').append(locations[currentLocation].buttonchoice.choice3);
     console.log(currentLocation);
     
 }
